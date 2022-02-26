379ec4794aa97291d4487c1884e6befa2d3f4f2d
krisnafirdaus <krisnafirdaus@gmail.com>
2022-02-26 10:42:18 +0800

    dashboard feature

--- controllers/index.js
+++ controllers/index.js
@@ -2,4 +2,5 @@ module.exports = {
   home: (req, res) => res.render("pages/default/home"),
   notFound: (req, res) => res.render("pages/default/not-found"),
   exception: (req, res) => res.render("pages/default/error"),
+  dashboard: require("./dashboard"),
 };
--- router/index.js
+++ router/index.js
@@ -5,4 +5,8 @@ router.get("/", controller.home);
 router.use(controller.notFound);
 router.use(controller.exception);
 
+const dashboard = require("./dashboard");
+
+router.use("/dashboard", dashboard);
+
 module.exports = router;
